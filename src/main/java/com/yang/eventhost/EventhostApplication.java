e2adfbc3ecaee50977c4dfbdd826412aa1fdd60a
jyang001 <joeyang095@gmail.com>
2019-01-08 16:09:28 -0500

    Initial commit

---
+++ src/main/java/com/yang/eventhost/EventhostApplication.java
@@ -0,0 +1,14 @@
+package com.yang.eventhost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventhostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventhostApplication.class, args);
+	}
+
+}
+

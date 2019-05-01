f814d3307e96fae66b5b606b68fce9791787886e
jyang001 <joeyang095@gmail.com>
2019-04-30 23:41:50 -0400

    modify WebSecurity allow access to css, js, and img files

--- src/main/java/com/yang/eventhost/security/WebSecurityConfig.java
+++ src/main/java/com/yang/eventhost/security/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -27,6 +28,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Qualifier("userDetailsServiceImpl")
 	private UserDetailsServiceImpl userDetailsServiceImpl;
 
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/javax.faces.resource/**");
+	}
+
 	/**
 	 * Crates bean of 'BCryptPasswordEncoder' to use to encrypt passwords
 	 * @return BCryptPasswordEncoder object
@@ -46,10 +52,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	/**
 	 * determines which routes can be accessed and also manages login
 	 */
-	@Override
-    protected void configure(HttpSecurity http) throws Exception {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
 
-		http.authorizeRequests().antMatchers("/resources/**", "/static/**","/webjars/**","/","/user/signup","/user/login").permitAll();
+		http.authorizeRequests().antMatchers("/resources/**","/css/**","/js/**","/img/**","/static/**","/webjars/**","/","/user/signup","/user/login").permitAll();
 
 		http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
 
